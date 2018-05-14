b491dca81207babcc8f1e2c6c446471e11d50759
Mystery00 <mystery0dyl520@gmail.com>
2018-05-15 01:52:41 +0800

    修复无法注销

--- helpers/func.php
+++ helpers/func.php
@@ -3,6 +3,17 @@
 function check_cookie(Response $response)
 {
     session_start();
+    if (isset($_SESSION['expire_time']))
+        if ($_SESSION['expire_time'] < time()) {
+            unset($_SESSION['user_id']);
+            unset($_SESSION['user_type']);
+            unset($_SESSION['link_id']);
+            unset($_SESSION['expire_time']);
+            $response->format($response->NOT_LOGIN);
+            return_data($response);
+        } else
+            $_SESSION['expire_time'] = time() + 60 * 10;
+
     if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_type'])) {
         $response->format($response->NOT_LOGIN);
         return_data($response);
